0170e57b5ef33c82ba801ffd3b72ab1ec043d813
fs203 <fs2031@icloud.com>
2024-08-15 14:47:30 +0900

    update

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -3,22 +3,21 @@
 // let random = Math.random();
 // if (random < 0.5) {
 //     console.log('数字は0.5より小さい');
-//     console.log(random);
-// }
-
-// if (random >= 0.5) {
+// } else {
 //     console.log('数字は0.5以上');
-//     console.log(random);
 // }
+// console.log(random);
 
-// const dayOfWeek = 'Friday';
+// const dayOfWeek = prompt('英語で曜日を入力してください：').toLowerCase();
 
 // if (dayOfWeek === 'Monday') {
 //     console.log('月曜は憂鬱');
 // } else if (dayOfWeek === 'Saturday') {
 //     console.log('土曜日は最高');
 // } else if (dayOfWeek === 'Friday') {
-//     console.log('仕事が終わってからの華金は最高')
+//     console.log('仕事が終わってからの華金は最高');
+// } else {
+//     console.log('はぁ。');
 // }
 
 // 0-5 無料
@@ -26,7 +25,7 @@
 // 10-65 大人料金
 // 65 over シニア料金
 
-const age = 3;
+const age = 100;
 
 if (age < 5) {
     console.log('無料です。');
@@ -34,4 +33,6 @@ if (age < 5) {
     console.log('子供料金なので1000円です。');
 } else if (age < 65) {
     console.log('大人料金なので2000円です。');
+} else {
+    console.log('シニア料金なので1000円です。');
 }
