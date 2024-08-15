a2adba6c77659107e82f1714b1c80e660146a572
fs203 <fs2031@icloud.com>
2024-08-15 11:07:58 +0900

    update

---
+++ Conditionals/app.js
@@ -0,0 +1,18 @@
+// console.log('条件分岐の前');
+
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log('数字は0.5より小さい');
+//     console.log(random);
+// }
+
+// if (random >= 0.5) {
+//     console.log('数字は0.5以上');
+//     console.log(random);
+// }
+
+const dayOfWeek = 'Monday';
+
+if (dayOfWeek === 'Monday') {
+    console.log('月曜は憂鬱');
+}
