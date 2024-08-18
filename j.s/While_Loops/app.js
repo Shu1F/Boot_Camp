31a9ceed9d0af63913c786d16fb01245541b3b18
fs203 <fs2031@icloud.com>
2024-08-18 21:00:36 +0900

    update

--- j.s/While_Loops/app.js
+++ j.s/While_Loops/app.js
@@ -19,7 +19,26 @@
 //     if (input === 'quit') break;
 // }
 
-for (let i = 0; i < 1000; i++) {
-    console.log(i);
-    if (i === 100) break;
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i);
+//     if (i === 100) break;
+// }
+
+let maximum = parseInt(prompt('数字を入力してください：'));
+while (!maximum) {
+    maximum = parseInt(prompt('エラーが起きました。有効な数字を入力してください：'));
 }
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt('1つ数字を決めました。数字を当ててみてください：'));
+while (guess !== targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt(prompt('その数字より小さいです！もう一度当ててみてください：'));
+    } else {
+        guess = parseInt(prompt('その数字より大きいです！もう一度当ててみてください：'));
+    }
+}
+
+console.log('正解！')
