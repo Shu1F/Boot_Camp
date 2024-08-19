c24082cb29e1aa250a011d8c2163e5cc6aa14d00
fs203 <fs2031@icloud.com>
2024-08-19 21:34:54 +0900

    update

---
+++ j.s/ToDo_Project/app.js
@@ -0,0 +1,23 @@
+let command = prompt('コマンドを入力してください(new, list, delete, quit)');
+
+while (command !== 'quit') {
+    let todos = [];
+    if (command === 'new') {
+        let newTodo = prompt('新しいTodoを入力してください');
+        todos.push(newTodo);
+        console.log(`「${newTodo}」を追加しました`)
+    } else if (command === 'list') {
+        console.log('*****************');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('*****************');
+    } else if (command === 'delete') {
+        let index = console.log('削除するインデックスを入力してください');
+        let deleted = todos.splice(index, 1);
+        console.log(`「{deleted[0]}」を削除しました`)
+    }
+    command = prompt('コマンドを入力してください(new, list, delete, quit)');
+}
+
+console.log('アプリを終了しました');
