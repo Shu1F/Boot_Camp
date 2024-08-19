1446cb2539368901e495b667dc9ccbbe864fea17
fs203 <fs2031@icloud.com>
2024-08-20 01:24:40 +0900

    update

--- j.s/ToDo_Project/ToDo_start/app.js
+++ j.s/ToDo_Project/ToDo_start/app.js
@@ -1,21 +1,21 @@
 let command = prompt('コマンドを入力してください(new, list, delete, quit)');
+let todos = [];
 
 while (command !== 'quit') {
-    let todos = [];
     if (command === 'new') {
         let newTodo = prompt('新しいTodoを入力してください');
         todos.push(newTodo);
-        console.log(`「${newTodo}」を追加しました`)
+        console.log(`「${newTodo}」を追加しました`);
     } else if (command === 'list') {
         console.log('*****************');
         for (let i = 0; i < todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`)
+            console.log(`${i}: ${todos[i]}`);
         }
         console.log('*****************');
     } else if (command === 'delete') {
-        let index = console.log('削除するインデックスを入力してください');
+        let index = prompt('削除するインデックスを入力してください');
         let deleted = todos.splice(index, 1);
-        console.log(`「{deleted[0]}」を削除しました`)
+        console.log(`「${deleted[0]}」を削除しました`);
     }
     command = prompt('コマンドを入力してください(new, list, delete, quit)');
 }
