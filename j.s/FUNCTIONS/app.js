d164ada48adf5327e97402944201360f942cd6d0
fs203 <fs2031@icloud.com>
2024-08-22 17:30:07 +0900

    update

--- j.s/FUNCTIONS/app.js
+++ j.s/FUNCTIONS/app.js
@@ -1,23 +1,30 @@
-function singSong() {
-    console.log('ド');
-    console.log('レ');
-    console.log('ミ');
-}
+// function singSong() {
+//     console.log('ド');
+//     console.log('レ');
+//     console.log('ミ');
+// }
 
 // singSong();
 
-function greet(firstName, lastName) {
-    console.log(`Hi ${firstName} ${lastName[0]}.`);
-}
+// function greet(firstName, lastName) {
+//     console.log(`Hi ${firstName} ${lastName[0]}.`);
+// }
 
 // greet('Shuichi', 'Fujiike');
 
 // repeat('hi', 3);
 
-function repeat(str, numTimes) {
-    let result = '';
-    for (let i = 0; i < numTimes; i++) {
-        result += str;
+// function repeat(str, numTimes) {
+//     let result = '';
+//     for (let i = 0; i < numTimes; i++) {
+//         result += str;
+//     }
+//     console.log(result);
+// }
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
     }
-    console.log(result);
+    return x + y;
 }
