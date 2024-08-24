dbfd3a658e2ae18a78fdf0891ebb49860d8bfbdb
fs203 <fs2031@icloud.com>
2024-08-24 13:33:53 +0900

    update

---
+++ j.s/Scope/app.js
@@ -0,0 +1,52 @@
+// let total = 0;
+// function collect() {
+//     let total = 6;
+// }
+// console.log(total);
+// collect();
+// console.log(total);
+
+// let bird = 'アオサギ';
+
+// function birdWatch() {
+//     let bird = 'ムクドリ';
+// }
+
+// birdWatch();
+// console.log(bird);
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14;
+//     let msg = 'hogehoge';
+//     console.log(PI);
+// }
+
+// console.log(msg);
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = 'jjbcuiawfv';
+// }
+
+// function bankRobbery() {
+//     const heroes = ['スパイダーマン', 'スーパーマン', 'ブラックパンサー'];
+//     function help() {
+//         for (let hero of heroes) {
+//             console.log(`助けて、${hero}!!!`);
+//         }
+//     }
+//     help();
+// }
+
+// let x = 'あいうえお';
+// function hoge() {
+//     console.log(x);
+// }
+// function moge() {
+//     let x = 'かきくけこ';
+//     hoge();
+// }
+
+const add = function(x, y) {
+    return x + y;
+}
