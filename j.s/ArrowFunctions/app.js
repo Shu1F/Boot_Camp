0686ab36376990922228e22c14ee07ca05b5b1a4
fs203 <fs2031@icloud.com>
2024-08-25 12:48:45 +0900

    update

--- j.s/ArrowFunctions/app.js
+++ j.s/ArrowFunctions/app.js
@@ -6,10 +6,55 @@
 //     return x + y;
 // }
 
-const square = (num) => {
-    return num * num;
-}
+// const square = (num) => {
+//     return num * num;
+// }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () => (
+//     Math.floor(Math.random() * 6) + 1
+// );
+
+// const add = (x, y) => x + y;
+
+// const movies = [
+//     {
+//         title: 'Amadeus',
+//         score: 99
+//     },
+//     {
+//         title: 'Stand By Me',
+//         score: 85
+//     },
+//     {
+//         title: 'Parasite',
+//         score: 95
+//     },
+//     {
+//         title: 'Alien',
+//         score: 90
+//     }
+// ]
+
+// const newMovies = movies.map(function (movie) {
+//     return `${movie.title} - ${movie.score / 10}`;
+// })
+
+// const rollDie = () => (
+    //     Math.floor(Math.random() * 6) + 1
+    // );
+
+// const newMovies = movies.map(movie => (
+//     `${movie.title} - ${movie.score / 10}`
+// ))
+// console.log('やったー');
+// setTimeout(() => {
+//     console.log('おーい');
+// }, 3000);
+
+// console.log('ばいばい');
+
+const id = setInterval(() => {
+    console.log(Math.random());
+}, 2000);
+
+clearInterval();
