5374d2e4c6d4e2bf05fbfde87b0e5819a0dd94b5
fs203 <fs2031@icloud.com>
2024-08-28 22:21:43 +0900

    upodate

---
+++ New_J.S/Destructuring/app.js
@@ -0,0 +1,5 @@
+const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
+
+const highScore = scores[0];
+const secondScore = scores[1];
+const [gold, silver, bronze, ...rest] = scores;
