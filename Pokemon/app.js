0070d2fe1548597d5c96663883e73419d63cfb11
fs203 <fs2031@icloud.com>
2024-09-01 19:56:57 +0900

    update

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,23 +1,43 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const container = document.querySelector('#container');
+// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
+// for (let i = 1; i <= 151; i++) {
+//     const pokemon = document.createElement('div');
+//     pokemon.classList.add('pokemon');
 
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+//     const label = document.createElement('span');
+//     label.innerText = `#${i}`;
+//     const newImg = document.createElement('img');
+//     newImg.src = `${baseURL}${i}.png`
 
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon);
-}
+//     pokemon.appendChild(newImg);
+//     pokemon.appendChild(label);
+//     container.appendChild(pokemon);
+// }
 
 /* <div>
     <img></img>
     <span>#2</span>
 </div> */
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    
+    const numberLabel = document.createElement('span');
+    numberLabel.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+    container.appendChild(newImg);
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(numberLabel);
+    container.appendChild(pokemon);
+}
+
